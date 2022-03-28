5b6ef153a7658eba014efdcf4b6d37094ce11d68
Shahab Pasha <shahab.pasha@rejlers.se>
2022-03-28 13:36:20 +0200

    Signed-off-by: Shahab Pasha <shahab.pasha@rejlers.se>

---
+++ AISound.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 12:31:39 2022
+
+@author: sp7012
+"""
+import librosa
+audio_data = '1MG.wav'
+x , sr = librosa.load(audio_data)
+import IPython.display as ipd
+ipd.Audio(audio_data)
+import matplotlib.pyplot as plt
+import librosa.display
+plt.figure(figsize=(14, 5))
+librosa.display.waveshow(x, sr=sr)
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+plt.colorbar()
+import sklearn
+spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
+spectral_centroids.shape
+# Computing the time variable for visualization
+plt.figure(figsize=(12, 4))
+frames = range(len(spectral_centroids))
+t = librosa.frames_to_time(frames)
+# Normalising the spectral centroid for visualisation
+def normalize(x, axis=0):
+    return sklearn.preprocessing.minmax_scale(x, axis=axis)
+#Plotting the Spectral Centroid along the waveform
+librosa.display.waveshow(x, sr=sr, alpha=0.4)
+plt.plot(t, normalize(spectral_centroids), color='b')
+mfccs = librosa.feature.mfcc(x, sr)
+print(mfccs.shape)
+#Displaying  the MFCCs:
+plt.figure(figsize=(15, 7))
+librosa.display.specshow(mfccs, sr=sr, x_axis='time')
