6979b75d78f86c885032202ef0b02d85981f0774
Shahab Pasha <shahab.pasha@rejlers.se>
2022-04-21 15:16:52 +0200

    Signed-off-by: Shahab Pasha <shahab.pasha@rejlers.se>

--- AISound.py
+++ AISound.py
@@ -10,19 +10,43 @@ import librosa
 audio_data = 'M.wav'
 #Reading the amplitude and sampling rate
 x , sr = librosa.load(audio_data)
+
+#Define and implementation of the filters
+import numpy as np
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+
+
+def butter_lowpass(cutoff, fs, order):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+b,a=butter_lowpass(2000, sr, order=16)
+x=lfilter(b, a, x)
+
+
+
 import IPython.display as ipd
 ipd.Audio(audio_data)
 import matplotlib.pyplot as plt
 import librosa.display
 plt.figure(figsize=(14, 5))
 librosa.display.waveshow(x, sr=sr)
-X = librosa.stft(x)
+X = librosa.stft(x, n_fft=2048, hop_length=512, win_length=2048, window="hann", center=True, dtype=None, pad_mode="constant")
 Xdb = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+librosa.display.specshow(Xdb, sr=sr,x_axis='time', y_axis='hz')
+plt.ylim(top=3000)
 plt.colorbar()
 import sklearn
-spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
+spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)
 spectral_centroids.shape
 # Computing the time variable for visualization
 plt.figure(figsize=(12, 4))
@@ -32,12 +56,10 @@ t = librosa.frames_to_time(frames)
 def normalize(x, axis=0):
     return sklearn.preprocessing.minmax_scale(x, axis=axis)
 #Plotting the Spectral Centroid along the waveform
-librosa.display.waveshow(x, sr=sr, alpha=0.4)
+librosa.display.waveshow(x, sr=sr, alpha=0.5)
 plt.plot(t, normalize(spectral_centroids), color='b')
 mfccs = librosa.feature.mfcc(x, sr)
 print(mfccs.shape)
 #Displaying  the MFCCs:
 plt.figure(figsize=(15, 7))
-librosa.display.specshow(mfccs, sr=sr, x_axis='time')
-
-
+librosa.display.specshow(mfccs[0:5], sr=sr, x_axis='time')
